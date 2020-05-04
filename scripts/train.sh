ce64699d553e2adb1f01f58ede470c6c5cf3cc32
eminorhan <eo41@nyu.edu>
2020-05-04 11:18:54 -0400

    Update resnext models

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,12 +2,12 @@
 
 #SBATCH --nodes=1
 ##SBATCH --ntasks=1
-#SBATCH --exclude=hpc1,hpc2,hpc3,hpc4,hpc5,hpc6,hpc7,hpc8,hpc9,vine3,vine4,vine6,vine11,vine12,lion17,rose7,rose8,rose9
-#SBATCH --gres=gpu:2
+#SBATCH --exclude=hpc1,hpc2,hpc3,hpc4,hpc5,hpc6,hpc7,hpc8,hpc9,vine3,vine4,vine6,vine11,vine12,lion17,rose1,rose2,rose3,rose4,rose7,rose8,rose9,lion3
+#SBATCH --gres=gpu:4
 ##SBATCH --cpus-per-task=1
 #SBATCH --mem=100GB
-#SBATCH --time=4:00:00
-#SBATCH --array=0-99
+#SBATCH --time=2:00:00
+#SBATCH --array=0-299
 #SBATCH --job-name=train
 #SBATCH --output=train_%A_%a.out
 
@@ -15,6 +15,6 @@ module purge
 module load cuda-10.0
 source /home/eo41/venv/bin/activate
 
-python -u /misc/vlgscratch4/LakeGroup/emin/bold5000/ROIs/train.py --model-name 'resnext101_32x8d_wsl' --subject 'CSI1' --freeze-trunk
+python -u /misc/vlgscratch4/LakeGroup/emin/bold5000/ROIs/train.py --model-name 'resnext101_32x8d_rand' --subject 'CSI1' --freeze-trunk
 
 echo "Done"
--- train.py
+++ train.py
@@ -17,10 +17,10 @@ import matplotlib as mp
 
 
 parser = argparse.ArgumentParser(description='Train models on fMRI data')
-parser.add_argument('--workers', default=2, type=int, help='number of data loading workers (default:4)')
-parser.add_argument('--epochs', default=100, type=int, help='number of total epochs to run')
-parser.add_argument('--batch-size', default=64, type=int, help='mini-batch size (default: 732), this is the total batch'
-                                                                ' size of all GPUs on the current node when using Data '
+parser.add_argument('--workers', default=8, type=int, help='number of data loading workers (default:4)')
+parser.add_argument('--epochs', default=50, type=int, help='number of total epochs to run')
+parser.add_argument('--batch-size', default=164, type=int, help='mini-batch size, this is the total batch size of all '
+                                                                'GPUs on the current node when using Data '
                                                                 'Parallel or Distributed Data Parallel')
 parser.add_argument('--lr', default=0.0005, type=float, help='learning rate')
 parser.add_argument('--weight-decay', default=0.0001, type=float, help='weight decay (default: 0)')
@@ -102,56 +102,81 @@ def load_model(args):
     # Ugly case by case (yikes Pytorch!)
     if args.layer == 0:
         layer_list = child_list[:5]
-        layer_list.append(torch.nn.AdaptiveAvgPool2d(output_size=(6, 6)))
-        in_dim = 9216
+        layer_list.append(torch.nn.AdaptiveAvgPool2d(output_size=(1, 1)))
+        in_dim = 256
     elif args.layer == 1:
         layer_list = child_list[:6]
-        layer_list.append(torch.nn.AdaptiveAvgPool2d(output_size=(4, 4)))
-        in_dim = 8192
+        layer_list.append(torch.nn.AdaptiveAvgPool2d(output_size=(1, 1)))
+        layer_list.append(Flatten())
+        layer_list.append(torch.nn.Linear(in_features=512, out_features=256, bias=False))
+        in_dim = 256
     elif args.layer == 2:
         top = child_list[6][:5]
         layer_list = child_list[:6] + list(top.children())
-        layer_list.append(torch.nn.AdaptiveAvgPool2d(output_size=(3, 3)))
-        in_dim = 9216
+        layer_list.append(torch.nn.AdaptiveAvgPool2d(output_size=(1, 1)))
+        layer_list.append(Flatten())
+        layer_list.append(torch.nn.Linear(in_features=1024, out_features=256, bias=False))
+        in_dim = 256
     elif args.layer == 3:
         top = child_list[6][:11]
         layer_list = child_list[:6] + list(top.children())
-        layer_list.append(torch.nn.AdaptiveAvgPool2d(output_size=(3, 3)))
-        in_dim = 9216
+        layer_list.append(torch.nn.AdaptiveAvgPool2d(output_size=(1, 1)))
+        layer_list.append(Flatten())
+        layer_list.append(torch.nn.Linear(in_features=1024, out_features=256, bias=False))
+        in_dim = 256
     elif args.layer == 4:
         top = child_list[6][:17]
         layer_list = child_list[:6] + list(top.children())
-        layer_list.append(torch.nn.AdaptiveAvgPool2d(output_size=(3, 3)))
-        in_dim = 9216
+        layer_list.append(torch.nn.AdaptiveAvgPool2d(output_size=(1, 1)))
+        layer_list.append(Flatten())
+        layer_list.append(torch.nn.Linear(in_features=1024, out_features=256, bias=False))
+        in_dim = 256
     elif args.layer == 5:
         top = child_list[6][:23]
         layer_list = child_list[:6] + list(top.children())
-        layer_list.append(torch.nn.AdaptiveAvgPool2d(output_size=(3, 3)))
-        in_dim = 9216
+        layer_list.append(torch.nn.AdaptiveAvgPool2d(output_size=(1, 1)))
+        layer_list.append(Flatten())
+        layer_list.append(torch.nn.Linear(in_features=1024, out_features=256, bias=False))
+        in_dim = 256
     elif args.layer == 6:
         top = child_list[7][:2]
         layer_list = child_list[:7] + list(top.children())
-        layer_list.append(torch.nn.AdaptiveAvgPool2d(output_size=(2, 2)))
-        in_dim = 8192
+        layer_list.append(torch.nn.AdaptiveAvgPool2d(output_size=(1, 1)))
+        layer_list.append(Flatten())
+        layer_list.append(torch.nn.Linear(in_features=2048, out_features=256, bias=False))
+        in_dim = 256
     elif args.layer == 7:
         top = child_list[7][:3]
         layer_list = child_list[:7] + list(top.children())
-        layer_list.append(torch.nn.AdaptiveAvgPool2d(output_size=(2, 2)))
-        in_dim = 8192
+        layer_list.append(torch.nn.AdaptiveAvgPool2d(output_size=(1, 1)))
+        layer_list.append(Flatten())
+        layer_list.append(torch.nn.Linear(in_features=2048, out_features=256, bias=False))
+        in_dim = 256
     elif args.layer == 8:
         layer_list = child_list[:9]
-        in_dim = 2048
+        layer_list.append(Flatten())
+        layer_list.append(torch.nn.Linear(in_features=2048, out_features=256, bias=False))
+        in_dim = 256
     elif args.layer == 9:
         model = LastLayerModel(model)
         layer_list = list(model.children())
-        in_dim = 1000
+        layer_list.append(Flatten())
+        layer_list.append(torch.nn.Linear(in_features=1000, out_features=256, bias=False))
+        in_dim = 256
     else:
         raise ValueError('Not supported.')
 
     layer_list.append(Flatten())
 
-    out_dim_dict = {'LHPPA': 131, 'RHEarlyVis': 285, 'LHRSC': 86, 'RHRSC': 143, 'LHLOC': 152, 'RHOPA': 187,
-                    'LHEarlyVis': 210, 'LHOPA': 101, 'RHPPA': 200, 'RHLOC': 190}
+    if args.subject == 'CSI1':
+        out_dim_dict = {'LHPPA': 131, 'RHEarlyVis': 285, 'LHRSC': 86, 'RHRSC': 143, 'LHLOC': 152, 'RHOPA': 187,
+                        'LHEarlyVis': 210, 'LHOPA': 101, 'RHPPA': 200, 'RHLOC': 190}
+    elif args.subject == 'CSI2':
+        out_dim_dict = {'LHPPA': 172, 'RHEarlyVis': 241, 'LHRSC': 59, 'RHRSC': 278, 'LHLOC': 327, 'RHOPA': 95,
+                        'LHEarlyVis': 254, 'LHOPA': 85, 'RHPPA': 198, 'RHLOC': 561}
+    elif args.subject == 'CSI3':
+        out_dim_dict = {'LHPPA': 112, 'RHEarlyVis': 696, 'LHRSC': 78, 'RHRSC': 116, 'LHLOC': 430, 'RHOPA': 205,
+                        'LHEarlyVis': 522, 'LHOPA': 187, 'RHPPA': 161, 'RHLOC': 597}
 
     layer_list.append(torch.nn.Linear(in_dim, out_dim_dict[args.region]))
     model = torch.nn.Sequential(*layer_list)
@@ -174,7 +199,7 @@ def dataset_generator(data_file, region):
 
     # generate train
     train_x = data['train_x']
-    train_y = data['train_y'].item()[region]  # make this generic
+    train_y = data['train_y'].item()[region]
 
     train_x_tensor = []
     for i in range(train_x.shape[0]):
@@ -189,7 +214,7 @@ def dataset_generator(data_file, region):
 
     # generate test
     test_x = data['test_x']
-    test_y = data['test_y'].item()[region]  # make this generic
+    test_y = data['test_y'].item()[region]
 
     test_x_tensor = []
     for i in range(test_x.shape[0]):
@@ -202,7 +227,22 @@ def dataset_generator(data_file, region):
     test_y_tensor = torch.from_numpy(test_y).float()
     test_dataset = torch.utils.data.TensorDataset(test_x_tensor, test_y_tensor)
 
-    return train_dataset, test_dataset
+    # generate val
+    val_x = data['val_x']
+    val_y = data['val_y'].item()[region]
+
+    val_x_tensor = []
+    for i in range(val_x.shape[0]):
+        val_x_tensor.append(
+            transforms.functional.normalize(transforms.functional.to_tensor(val_x[i]),
+                                            mean=[0.485, 0.456, 0.406],
+                                            std=[0.229, 0.224, 0.225]).view(-1, 3, 224, 224))
+
+    val_x_tensor = torch.cat(val_x_tensor, dim=0)
+    val_y_tensor = torch.from_numpy(val_y).float()
+    val_dataset = torch.utils.data.TensorDataset(val_x_tensor, val_y_tensor)
+
+    return train_dataset, test_dataset, val_dataset
 
 
 def train(model, criterion, optimizer, train_loader):
@@ -299,11 +339,14 @@ if __name__ == "__main__":
     job_idx = int(os.getenv('SLURM_ARRAY_TASK_ID'))
     regions = ['LHPPA', 'RHEarlyVis', 'LHRSC', 'RHRSC', 'LHLOC', 'RHOPA', 'LHEarlyVis', 'LHOPA', 'RHPPA', 'RHLOC']
     layers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    RR, LL = np.meshgrid(regions, layers)
+    rhos = [0., 5e-5, 5e-4]
+    RR, LL, PP = np.meshgrid(regions, layers, rhos)
     RR = RR.flatten()
     LL = LL.flatten()
+    PP = PP.flatten()
     args.region = RR[job_idx]
     args.layer = LL[job_idx]
+    args.weight_decay = PP[job_idx]
     # #######################
 
     torch_hub_dir = '/misc/vlgscratch4/LakeGroup/emin/robust_vision/pretrained_models'
@@ -312,38 +355,50 @@ if __name__ == "__main__":
     model = load_model(args)
 
     data_file = '../data/' + args.subject + '_data.npz'
-    train_dataset, test_dataset = dataset_generator(data_file, args.region)
+    train_dataset, test_dataset, val_dataset = dataset_generator(data_file, args.region)
 
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True,
                                                num_workers=args.workers, pin_memory=True, sampler=None)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False,
                                               num_workers=args.workers, pin_memory=True, sampler=None)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False,
+                                             num_workers=args.workers, pin_memory=True, sampler=None)
 
     tr_losses = np.zeros(args.epochs)
     test_losses = np.zeros(args.epochs)
+    val_losses = np.zeros(args.epochs)
 
     # define loss function (criterion) and optimizer
     criterion = torch.nn.L1Loss().cuda()
     optimizer = torch.optim.Adam(model.parameters(), args.lr, weight_decay=args.weight_decay)
 
-    best_test_loss = 1000000
+    best_val_loss = 1000000
 
     for i in range(args.epochs):
         tr_loss = train(model, criterion, optimizer, train_loader)
-        test_loss, output, target = validate(model, criterion, test_loader)
+        test_loss, test_output, test_target = validate(model, criterion, test_loader)
+        val_loss, val_output, val_target = validate(model, criterion, val_loader)
 
         tr_losses[i] = tr_loss
         test_losses[i] = test_loss
+        val_losses[i] = val_loss
 
-        if test_loss < best_test_loss:
-            best_output = output
-            best_target = target
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+
+            best_val_output = val_output
+            best_val_target = val_target
+
+            best_test_output = test_output
+            best_test_target = test_target
 
         print('End of epoch:', i + 1)
 
-    save_filename = args.subject + '_' + args.region + '_' +  str(args.layer) + '_' + args.model_name + '.tar'
+    save_filename = args.subject + '_' + args.region + '_' +  str(args.layer) + '_' + str(args.weight_decay) + '_' + \
+                    args.model_name + '.tar'
 
     # plot_preds(best_output, best_target)
 
-    np.savez(save_filename, tr_losses=tr_losses, test_losses=test_losses, best_output=best_output,
-             best_target=best_target)
+    np.savez(save_filename, tr_losses=tr_losses, test_losses=test_losses, val_losses=val_losses,
+             best_val_output=best_val_output, best_val_target=best_val_target,
+             best_test_output=best_test_output, best_test_target=best_test_target)
