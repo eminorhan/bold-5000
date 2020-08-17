6954d1b5e1fd78e9caaca4d7ed91b536e0cf6d80
eminorhan <eo41@nyu.edu>
2020-08-16 23:36:29 -0400

    improved plotting of results

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## fMRI signals from visual cortical areas are too noisy to distinguish between even drastically different image recognition models 
 
+To whom thus *Adam* cleerd of doubt, repli’d.
+How fully hast thou satisfi’d mee, pure
+Intelligence of Heav’n, Angel serene,
+And freed from intricacies, taught to live,
+The easiest way, nor with perplexing thoughts
+
+-- *Paradise Lost* (Book VII)
+
 This project is work in progress and aims to determine the minimal model capable of fully accounting for the variance observed in a large dataset of visual fMRI responses to natural images ([BOLD5000](https://bold5000.github.io/)). I am hoping to have a write-up of the results in the next couple of months.
--- analyze.py
+++ analyze.py
@@ -10,11 +10,11 @@ import matplotlib.pylab as plt
 import matplotlib as mp
 
 
-def plot_corrs(corrs, model_name):
+def plot_corrs(corrs, model_name, save_filename):
     '''To visualize prediction accuracy'''
     plt.clf()
 
-    regions = ['LHPPA', 'RHEarlyVis', 'LHRSC', 'RHRSC', 'LHLOC', 'RHOPA', 'LHEarlyVis', 'LHOPA', 'RHPPA', 'RHLOC']
+    regions = ['LHEarlyVis', 'LHRSC', 'LHPPA', 'LHOPA', 'LHLOC', 'RHEarlyVis', 'RHRSC', 'RHPPA', 'RHOPA', 'RHLOC']
 
     if model_name.startswith('alexnet'):
         layers = [0, 1, 2, 3, 4, 5, 6, 7, 8]
@@ -22,26 +22,67 @@ def plot_corrs(corrs, model_name):
         layers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     colors = ['r', 'b', 'k']
+    shade_colors = ['pink', 'lightskyblue']
+
+    ax = [None] * 10
 
     c_ind = 0
     for c in corrs:
         r_ind = 0
         for r in regions:
-            plt.subplot(2, 5, r_ind+1)
+            ax[r_ind] = plt.subplot(2, 5, r_ind+1)
             x = np.zeros(len(layers))
+            s = np.zeros(len(layers))
             for l in layers:
                 x[l] = np.mean(c[r, l])
-            plt.plot(layers, x, color=colors[c_ind])
+                s[l] = np.std(c[r, l]) / np.sqrt(len(c[r, l]))
+            plt.plot(layers, x, '-', color=colors[c_ind])
+            plt.plot(layers, x, '.', color=colors[c_ind])
+            plt.fill_between(layers, x - s, x + s, color=shade_colors[c_ind])
             plt.ylim([0, .25])
+            plt.title(r, fontsize=10)
+
+            if r_ind == 5:
+                if model_name.startswith('alexnet'):
+                    plt.yticks([0, .05, .1, .15, .2, .25], ['0', '0.05', '0.1', '0.15', '0.2', '0.25'])
+                    plt.xticks(layers, ['0', '1', '2', '3', '4', '5', '6', '7', '8'])
+                    plt.xlabel('Layers')
+                    plt.ylabel('Correlation')
+                    plt.text(0, 0.23, 'AlexNet-ImageNet', fontsize=8, color='r')
+                    plt.text(0, 0.21, 'AlexNet-Random', fontsize=8, color='b')
+                else:
+                    plt.yticks([0, .05, .1, .15, .2, .25], ['0', '0.05', '0.1', '0.15', '0.2', '0.25'])
+                    plt.xticks(layers, ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+                    plt.xlabel('Layers')
+                    plt.ylabel('Correlation')
+            else:
+                if model_name.startswith('alexnet'):
+                    plt.yticks([0, .05, .1, .15, .2, .25], ['', '', '', '', '', ''])
+                    plt.xticks(layers, ['', '', '', '', '', '', '', '', ''])
+                else:
+                    plt.yticks([0, .05, .1, .15, .2, .25], ['', '', '', '', '', ''])
+                    plt.xticks(layers, ['', '', '', '', '', '', '', '', '', ''])
+
+            ax[r_ind].spines["right"].set_visible(False)
+            ax[r_ind].spines["top"].set_visible(False)
+            ax[r_ind].yaxis.set_ticks_position('left')
+            ax[r_ind].xaxis.set_ticks_position('bottom')
+
             r_ind += 1
         c_ind += 1
 
-    plt.savefig('freeze_alexnet_correlations_lowd_reg.pdf', bbox_inches='tight')
+    mp.rcParams['axes.linewidth'] = 0.75
+    mp.rcParams['patch.linewidth'] = 0.75
+    mp.rcParams['patch.linewidth'] = 1.15
+    mp.rcParams['font.sans-serif'] = ['FreeSans']
+    mp.rcParams['mathtext.fontset'] = 'cm'
+
+    plt.savefig(save_filename, bbox_inches='tight')
 
 
 def extract_model_results(directory, model_name):
     '''Results for one model'''
-    regions = ['LHPPA', 'RHEarlyVis', 'LHRSC', 'RHRSC', 'LHLOC', 'RHOPA', 'LHEarlyVis', 'LHOPA', 'RHPPA', 'RHLOC']
+    regions = ['LHEarlyVis', 'LHRSC', 'LHPPA', 'LHOPA', 'LHLOC', 'RHEarlyVis', 'RHRSC', 'RHPPA', 'RHOPA', 'RHLOC']
 
     if model_name.startswith('alexnet'):
         layers = [0, 1, 2, 3, 4, 5, 6, 7, 8]
@@ -104,10 +145,12 @@ def extract_model_results(directory, model_name):
 
 if __name__ == "__main__":
 
-    freeze_trunk_aleximgnet = extract_model_results('../fit_results/freeze_trunk/CSI1_aleximgnet/', 'alexnet_imgnet')
-    freeze_trunk_alexrand = extract_model_results('../fit_results/freeze_trunk/CSI1_alexrand/', 'alexnet_rand')
+    freeze_trunk_aleximgnet = extract_model_results('../fit_results/freeze_trunk/CSI1/CSI1_aleximgnet/', 'alexnet_imgnet')
+    freeze_trunk_alexrand = extract_model_results('../fit_results/freeze_trunk/CSI1/CSI1_alexrand/', 'alexnet_rand')
 
-    train_trunk_aleximgnet = extract_model_results('../fit_results/train_trunk/CSI1_aleximgnet/', 'alexnet_imgnet')
-    train_trunk_alexrand = extract_model_results('../fit_results/train_trunk/CSI1_alexrand/', 'alexnet_rand')
+    train_trunk_aleximgnet = extract_model_results('../fit_results/train_trunk/CSI1/CSI1_aleximgnet/', 'alexnet_imgnet')
+    train_trunk_alexrand = extract_model_results('../fit_results/train_trunk/CSI1/CSI1_alexrand/', 'alexnet_rand')
 
-    plot_corrs([freeze_trunk_aleximgnet, freeze_trunk_alexrand], 'alexnet')
+    save_filename = 'alexnet.pdf'
+
+    plot_corrs([freeze_trunk_aleximgnet, freeze_trunk_alexrand], 'alexnet', save_filename=save_filename)
