f6ea5d28a429afbae567d494b6d59c44d49b0da7
eminorhan <eo41@nyu.edu>
2020-08-17 11:17:15 -0400

    plot resnext results

--- analyze.py
+++ analyze.py
@@ -21,8 +21,8 @@ def plot_corrs(corrs, model_name, save_filename):
     else:
         layers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    colors = ['r', 'b', 'k']
-    shade_colors = ['pink', 'lightskyblue']
+    colors = ['r', 'b', 'm', 'g', 'k']
+    shade_colors = ['pink', 'lightskyblue', 'plum', 'lightgreen', 'lightgray']
 
     ax = [None] * 10
 
@@ -36,31 +36,41 @@ def plot_corrs(corrs, model_name, save_filename):
             for l in layers:
                 x[l] = np.mean(c[r, l])
                 s[l] = np.std(c[r, l]) / np.sqrt(len(c[r, l]))
-            plt.plot(layers, x, '-', color=colors[c_ind])
-            plt.plot(layers, x, '.', color=colors[c_ind])
-            plt.fill_between(layers, x - s, x + s, color=shade_colors[c_ind])
-            plt.ylim([0, .25])
+
+            if model_name.startswith('alexnet'):
+                plt.plot(layers, x, '-', color=colors[c_ind])
+                plt.plot(layers, x, '.', color=colors[c_ind])
+                plt.fill_between(layers, x - s, x + s, color=shade_colors[c_ind])
+            else:
+                plt.plot(layers, x, '-', color=colors[c_ind+2])
+                plt.plot(layers, x, '.', color=colors[c_ind+2])
+                plt.fill_between(layers, x - s, x + s, color=shade_colors[c_ind+2])
+
+            plt.ylim([0, .3])
             plt.title(r, fontsize=10)
 
             if r_ind == 5:
                 if model_name.startswith('alexnet'):
-                    plt.yticks([0, .05, .1, .15, .2, .25], ['0', '0.05', '0.1', '0.15', '0.2', '0.25'])
+                    plt.yticks([0, .1, .2, .3], ['0', '0.1', '0.2', '0.3'])
                     plt.xticks(layers, ['0', '1', '2', '3', '4', '5', '6', '7', '8'])
                     plt.xlabel('Layers')
                     plt.ylabel('Correlation')
-                    plt.text(0, 0.23, 'AlexNet-ImageNet', fontsize=8, color='r')
-                    plt.text(0, 0.21, 'AlexNet-Random', fontsize=8, color='b')
+                    plt.text(0, 0.28, 'AlexNet-ImageNet', fontsize=8, color='r')
+                    plt.text(0, 0.26, 'AlexNet-Random', fontsize=8, color='b')
                 else:
-                    plt.yticks([0, .05, .1, .15, .2, .25], ['0', '0.05', '0.1', '0.15', '0.2', '0.25'])
+                    plt.yticks([0, .1, .2, .3], ['0', '0.1', '0.2', '0.3'])
                     plt.xticks(layers, ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
                     plt.xlabel('Layers')
                     plt.ylabel('Correlation')
+                    plt.text(0, 0.29, 'ResNeXt-WSL', fontsize=8, color='m')
+                    plt.text(0, 0.27, 'ResNeXt-ImageNet', fontsize=8, color='g')
+                    plt.text(0, 0.25, 'ResNeXt-Random', fontsize=8, color='k')
             else:
                 if model_name.startswith('alexnet'):
-                    plt.yticks([0, .05, .1, .15, .2, .25], ['', '', '', '', '', ''])
+                    plt.yticks([0, .1, .2, .3], ['', '', '', ''])
                     plt.xticks(layers, ['', '', '', '', '', '', '', '', ''])
                 else:
-                    plt.yticks([0, .05, .1, .15, .2, .25], ['', '', '', '', '', ''])
+                    plt.yticks([0, .1, .2, .3], ['', '', '', ''])
                     plt.xticks(layers, ['', '', '', '', '', '', '', '', '', ''])
 
             ax[r_ind].spines["right"].set_visible(False)
@@ -145,15 +155,37 @@ def extract_model_results(directory, model_name):
 
 if __name__ == "__main__":
 
-    freeze_trunk_aleximgnet = extract_model_results('../fit_results/freeze_trunk/CSI1/CSI1_aleximgnet/', 'alexnet_imgnet')
+    freeze_trunk_aleximgnet = extract_model_results('../fit_results/freeze_trunk/CSI1/CSI1_aleximgnet/',
+                                                    'alexnet_imgnet')
     freeze_trunk_alexrand = extract_model_results('../fit_results/freeze_trunk/CSI1/CSI1_alexrand/', 'alexnet_rand')
 
     train_trunk_aleximgnet = extract_model_results('../fit_results/train_trunk/CSI1/CSI1_aleximgnet/', 'alexnet_imgnet')
     train_trunk_alexrand = extract_model_results('../fit_results/train_trunk/CSI1/CSI1_alexrand/', 'alexnet_rand')
 
+    freeze_trunk_resnextwsl = extract_model_results('../fit_results/freeze_trunk/CSI1/CSI1_resnextwsl/',
+                                                    'resnext101_32x8d_wsl')
+    freeze_trunk_resnextimgnet = extract_model_results('../fit_results/freeze_trunk/CSI1/CSI1_resnextimgnet/',
+                                                       'resnext101_32x8d_imgnet')
+    freeze_trunk_resnextrand = extract_model_results('../fit_results/freeze_trunk/CSI1/CSI1_resnextrand/',
+                                                     'resnext101_32x8d_rand')
+
+    train_trunk_resnextwsl = extract_model_results('../fit_results/train_trunk/CSI1/CSI1_resnextwsl/',
+                                                   'resnext101_32x8d_wsl')
+    train_trunk_resnextimgnet = extract_model_results('../fit_results/train_trunk/CSI1/CSI1_resnextimgnet/',
+                                                      'resnext101_32x8d_imgnet')
+    train_trunk_resnextrand = extract_model_results('../fit_results/train_trunk/CSI1/CSI1_resnextrand/',
+                                                    'resnext101_32x8d_rand')
+
+    # 1. plot AlexNet results
     frozen_save_filename = 'frozen_alexnet.pdf'
     train_save_filename = 'train_alexnet.pdf'
-
     plot_corrs([freeze_trunk_aleximgnet, freeze_trunk_alexrand], 'alexnet', save_filename=frozen_save_filename)
     plot_corrs([train_trunk_aleximgnet, train_trunk_alexrand], 'alexnet', save_filename=train_save_filename)
 
+    # 2. plot ResNeXt results
+    frozen_save_filename = 'frozen_resnext.pdf'
+    train_save_filename = 'train_resnext.pdf'
+    plot_corrs([freeze_trunk_resnextwsl, freeze_trunk_resnextimgnet, freeze_trunk_resnextrand], 'resnext',
+               save_filename=frozen_save_filename)
+    plot_corrs([train_trunk_resnextwsl, train_trunk_resnextimgnet, train_trunk_resnextrand], 'resnext',
+               save_filename=train_save_filename)
